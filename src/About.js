34c5359dae0b2a2c7071afc7e10ffc38313a4ae9
rich.sykes <rich.sykes@hotmail.co.uk>
2020-01-01 13:23:32 +0000

    init

---
+++ src/About.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import './App.css';
+
+function About() {
+  return (
+    <div>
+      <h1>Risk Party!</h1>
+      <h3>About Page</h3>
+    </div>
+  );
+}
+
+export default About;
---
+++ src/Contact.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import './App.css';
+
+function Contact() {
+  return (
+    <div>
+      <h1>Risk Party!</h1>
+      <h3>Contact Page</h3>
+    </div>
+  );
+}
+
+export default Contact;
